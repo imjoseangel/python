1931f6f1c3dec0f24fc1dcfbbfccd7cb7939cca4
imjoseangel <josea.munoz@gmail.com>
2015-02-05 17:20:54 +0100

    EML File Subject Decoder
    Decodes Subject in eml file

--- decrypt_subject_eml.py
+++ decrypt_subject_eml.py
@@ -4,4 +4,4 @@ import sys
 from email.Header import decode_header
 
 s = decode_header(str(sys.argv[1]))
-print( s[0][0].decode('iso-8859-1').encode('utf-8'))
+print( s[0][0].decode('iso-8859-1').encode('utf-8')) 
