3caa69a6813454602950013612dc5634b7c18247
imjoseangel <josea.munoz@gmail.com>
2015-08-26 16:19:59 +0200

    Telegram

---
+++ telegram_bot.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import telebot
+TOKEN = '120822365:AAGOoLYW8mqKboGKSEvZ5YUYOOxsgVuLlXM'
+bot = telebot.TeleBot(TOKEN)
+
+# Handle /start and /help
+@bot.message_handler(commands=['start', 'help'])
+def command_help(message):
+    bot.reply_to(message, "Hello, did someone call for help?")
+
+# Handles all messages which text matches the regex regexp.
+# See https://en.wikipedia.org/wiki/Regular_expression
+# This regex matches all sent url's.
+@bot.message_handler(regexp='((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
+def command_url(message):
+    bot.reply_to(message, "I shouldn't open that url, should I?")
+
+# Handle all sent documents of type 'text/plain'.
+@bot.message_handler(func=lambda message: message.document.mime_type == 'text/plain', content_types=['document'])
+def command_handle_document(message):
+    bot.reply_to(message, "Document received, sir!")
+
+# Default command handler. A lambda expression which always returns True is used for this purpose.
+@bot.message_handler(func=lambda message: True, content_types=['audio', 'video', 'document', 'text', 'location', 'contact', 'sticker'])
+def default_command(message):
+    bot.reply_to(message, "This is the default command handler.")
+
+bot.polling()
+
+while True: # Don't end the main thread.
+    pass
