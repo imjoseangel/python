0790da31217a9074fbeaa1cd70f570b6c3a4cc52
imjoseangel <josea.munoz@gmail.com>
2015-09-02 14:48:09 +0200

    Update post_https.py

--- post_https.py
+++ post_https.py
@@ -3,9 +3,9 @@
 import urllib2
 import urllib
 
-url = 'https://10.100.20.94/Sourcing/inspector'
+url = 'https://192.168.0.1'
 username = 'debug'
-password = 'gr1t4nd0_L1br3*'
+password = 'password'
 
 p = urllib2.HTTPPasswordMgrWithDefaultRealm()
 
@@ -15,7 +15,7 @@ handler = urllib2.HTTPBasicAuthHandler(p)
 opener = urllib2.build_opener(handler)
 urllib2.install_opener(opener)
 
-urlaql = 'https://10.100.20.94/Sourcing/inspector/aql'
+urlaql = 'https://192.168.0.1/aql'
 
 post_data = {'restrictQueryBy': 'None', 'query': 'SELECT this, Dataloads.TimeScheduled, Name, Dataloads.TimeStarted,Dataloads.TimeCompleted, Dataloads.NodeName FROM ariba.analytics.server.DataloadSchedule include inactive ORDER BY Dataloads.TimeStarted desc,Dataloads.TimeScheduled desc', 'partitionNames': 'Any', 'acrossCommunities': 'off', 'baseDomainVariant': 'off', 'joinType': 3, 'maxRecords': 500, 'userLocale': 'English', 'userPartition': 'Any', 'submit': 'Submit'}
 
