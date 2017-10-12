8b8e1c585ab443ec0b590e85c79dc7357e310ff0
Jose Angel <imjoseangel@users.noreply.github.com>
2017-10-12 13:33:26 +0200

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,32 +1,87 @@
 #!/usr/bin/env python
 
-import telebot
-TOKEN = '108270433:AAHGe0GZv4dqla-m4l07IgiNhhRBGsdkyWQ'
-bot = telebot.TeleBot(TOKEN)
+# Import Modules
+import os
+import time
+import sys
+import subprocess
+from functools import wraps
+from telegram.ext import CommandHandler
 
-# Handle /start and /help
-@bot.message_handler(commands=['start', 'help'])
-def command_help(message):
-    bot.reply_to(message, "Hello, did someone call for help?")
+# Restrict Access
+LIST_OF_ADMINS = [423525222]
 
-# Handles all messages which text matches the regex regexp.
-# See https://en.wikipedia.org/wiki/Regular_expression
-# This regex matches all sent url's.
-@bot.message_handler(regexp='((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)')
-def command_url(message):
-    bot.reply_to(message, "I shouldn't open that url, should I?")
+# First, you have to create an Updater object. Replace 'TOKEN' with your Bot's API token.
 
-# Handle all sent documents of type 'text/plain'.
-@bot.message_handler(func=lambda message: message.document.mime_type == 'text/plain', content_types=['document'])
-def command_handle_document(message):
-    bot.reply_to(message, "Document received, sir!")
+from telegram.ext import Updater
+updater = Updater(token='xxx')
 
-# Default command handler. A lambda expression which always returns True is used for this purpose.
-@bot.message_handler(func=lambda message: True, content_types=['audio', 'video', 'document', 'text', 'location', 'contact', 'sticker'])
-def default_command(message):
-    bot.reply_to(message, "This is the default command handler.")
+# For quicker access to the Dispatcher used by your Updater, you can introduce it locally:
 
-bot.polling()
+dispatcher = updater.dispatcher
 
-while True: # Don't end the main thread.
-    pass
+# This is a good time to set up the logging module, so you will know when (and why) things don't work as expected:
+
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
+# Now, you can define a function that should process a specific type of update:
+
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Hey Jose, I'm your bot, please talk to me!")
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+def help(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="/help - Prints this Help\n/ip - Prints External IP\n/restart - Restarts Bot\n/run - Runs a Command\n/start - Hello Message\n/who - Who is connected")
+start_handler = CommandHandler('help', help)
+dispatcher.add_handler(start_handler)
+
+
+
+# List of Functions
+
+def restricted(func):
+    @wraps(func)
+    def wrapped(bot, update, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in LIST_OF_ADMINS:
+            print("Unauthorized access denied for {}.".format(user_id))
+            return
+        return func(bot, update, *args, **kwargs)
+    return wrapped
+
+@restricted
+def restart(bot, update):
+    bot.send_message(update.message.chat_id, "Bot is restarting...")
+    time.sleep(0.2)
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+dispatcher.add_handler(CommandHandler('restart', restart))
+
+@restricted
+def who(bot, update):
+    time.sleep(0.2)
+    details = subprocess.check_output(['w'])
+    bot.send_message(update.message.chat_id, details)
+
+dispatcher.add_handler(CommandHandler('who', who))
+
+@restricted
+def run(bot, update, args):
+    time.sleep(0.2)
+    details = subprocess.check_output(args)
+    bot.send_message(update.message.chat_id, details)
+dispatcher.add_handler(CommandHandler('run', run, pass_args=True))
+
+@restricted
+def ip(bot, update):
+    time.sleep(0.2)
+    details = subprocess.check_output(['/usr/bin/curl', 'ipinfo.io'])
+    bot.send_message(update.message.chat_id, details)
+
+dispatcher.add_handler(CommandHandler('ip', ip))
+
+# And that's all you need. To start the bot, run:
+updater.start_polling()
+updater.idle()
