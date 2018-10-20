32a7fd5f4d4c20d37132ffcf3fce2a59b5db484e
Jose Angel Munoz <josea.munoz@gmail.com>
2018-10-20 15:36:56 +0200

    Update moleculecheck.py

--- ansible/moleculecheck.py
+++ ansible/moleculecheck.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import division, absolute_import, print_function, \
-    unicode_literals
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
 import imp
 import os
 import re
